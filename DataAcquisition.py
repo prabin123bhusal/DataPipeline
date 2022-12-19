f0e23b24f9389d8cd8b8a23eb5869459b7ea376d
prabin123bhusal <55730269+prabin123bhusal@users.noreply.github.com>
2022-12-19 18:02:37 +0530

    Update DataAcquisition.py

--- DataAcquisition.py
+++ DataAcquisition.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
+
 
 # # Data Acquisition from API
-
-# In[23]:
 
 
 import requests
@@ -11,24 +8,12 @@ import csv
 
 from requests.api import head
 
-
-# In[24]:
-
-
 url = 'https://api.coincap.io/v2/assets'
-
-
-# In[25]:
-
 
 headers = {
     'Accept' : 'application/json',
     'Content-Type' : 'application/json'
 }
-
-
-# In[26]:
-
 
 response = requests.request("GET", url, headers=headers, data={})
 myjson = response.json()
@@ -36,24 +21,15 @@ ourdata = []
 csvheader = ['SYMBOL', 'NAME', 'PRICE(USD)']
 
 
-# In[28]:
-
-
 for x in myjson['data']:
     listing = [x['symbol'], x['name'], x['priceUsd']]
     ourdata.append(listing)
-
-
-# In[30]:
 
 
 with open('data.csv','w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(csvheader)
     writer.writerows(ourdata)
-
-
-# In[31]:
 
 
 print("done")
